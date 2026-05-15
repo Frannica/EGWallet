1c93b26ed6e8459025707caece817d58b3052211
Francisco Abomo <f.mba1994@gmail.com>
2026-05-15 01:16:44 -0400

    chore: bump versionCode to 50

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ export default ({ config }) => ({
     },
     edgeToEdgeEnabled: true,
     package: "com.francisco1953.egwalletmobile",
-    versionCode: 49
+    versionCode: 50
   },
   web: {
     favicon: "./assets/favicon.png"
